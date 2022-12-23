2874b279133e68711ed7f412a229445e8a68b3c2
arupadhyay25 <arupadhyay25@gmail.com>
2022-12-23 09:53:56 +0530

    jhgjh

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-require("dotenv").config()
-const connection=mongoose.connect(`mongodb+srv://r:Farman@cluster0.ksgue.mongodb.net/insightcentral?retryWrites=true&w=majority`,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-
-
-module.exports={connection}
